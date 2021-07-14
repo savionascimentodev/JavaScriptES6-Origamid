d27489e9f2b43000a0a34d09a69972a8363b0d63
Savio Nascimento <savionascimentodev@gmail.com>
2021-07-14 17:32:36 -0400

    ✨ scripts numeros e operadores

---
+++ JavaScript-Iniciantes/Numeros-e-operadores/script-aula.js
@@ -0,0 +1,23 @@
+var total = 10 + 5 * 2 / 2 + 20
+
+
+// Criar duas expressões que retornem NaN
+var nome = 'Savio' - 102
+var teste = 'Teste' / 5
+
+console.log(nome, teste)
+
+// Somar a string '200' com o número 50 e retornar 250
+var soma = +'200' + 50
+console.log(soma)
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var x = 5;
+console.log(++x);
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+
+console.log(peso);
--- JavaScript-Iniciantes/Numeros-e-operadores/script.js
+++ JavaScript-Iniciantes/Numeros-e-operadores/script.js
@@ -8,3 +8,39 @@ console.log(modulo)
 
 var testeNaN = 'isso é 100' / 2
 console.log(testeNaN)
+// É possível verificar se uma variável é NaN ou não com a função isNaN()
+
+var soma = 100 + 50; // 150
+var subtracao = 100 - 50; // 50
+var multiplicacao = 100 * 2; // 200
+var divisao = 100 / 2; // 50
+var expoente = 2 ** 4; // 16
+var modulo = 14 % 5; // 4
+// Lembrando que 'soma' + em Strings serve para concatenar
+
+
+// Incrementando
+var incremento = 5;
+console.log(incremento++); // 5
+console.log(incremento); // 6
+
+var incremento2 = 5;
+console.log(++incremento2); // 6
+console.log(incremento2); // 6
+// Mesma coisa para o decremento--x
+
+
+// O + e - tenta transformar o valor seguinte em número
+var frase = 'Isso é um teste';
++frase; // NaN
+-frase; // NaN
+
+var idade = '28';
++idade; // 28 (número)
+-idade; // -28 (número)
+console.log(+idade + 5); // 33 
+
+var possuiFaculdade = true;
+console.log(+possuiFaculdade); // 1
+
+// O - antes de um número torna ele negativo
